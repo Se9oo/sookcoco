86118158ad6db8c3f53c1e3ab8b86259ffced806
park se hyeon <se9oo@kakao.com>
2021-08-22 18:44:42 +0900

    common file 추가 (서버, 클래스 정보)

---
+++ src/common/common.js
@@ -0,0 +1,129 @@
+export const servers = [
+  { name: '루페온', value: 'lupeon' },
+  { name: '실리안', value: 'cilian' },
+  { name: '아만', value: 'aman' },
+  { name: '카마인', value: 'carmine' },
+  { name: '카제로스', value: 'caseros' },
+  { name: '아브렐슈드', value: 'abrelschud' },
+  { name: '카단', value: 'cardan' },
+  { name: '니나브', value: 'ninav' },
+];
+
+export const characters = [
+  // 전사
+  [
+    {
+      kor: '버서커',
+      eng: 'berserker',
+      src: 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/berserker_m.png',
+    },
+    {
+      kor: '워로드',
+      eng: 'warlord',
+      src: 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/warlord_m.png',
+    },
+    {
+      kor: '디스트로이어',
+      eng: 'destroyer',
+      src: 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/destroyer_m.png',
+    },
+    {
+      kor: '홀리나이트',
+      eng: 'holyknight',
+      src: 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/holyknight_m.png',
+    },
+  ],
+  // 무도가
+  [
+    {
+      kor: '배틀마스터',
+      eng: 'battlemaster',
+      src: 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/battlemaster_m.png',
+    },
+    {
+      kor: '인파이터',
+      eng: 'infighter',
+      src: 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/infighter_m.png',
+    },
+    {
+      kor: '기공사',
+      eng: 'soulmaster',
+      src: 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/soulmaster_m.png',
+    },
+    {
+      kor: '창술사',
+      eng: 'lancemaster',
+      src: 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/lancemaster_m.png',
+    },
+    {
+      kor: '스트라이커',
+      eng: 'striker',
+      src: 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/striker_m.png',
+    },
+  ],
+  // 헌터
+  [
+    {
+      kor: '데빌헌터',
+      eng: 'devilhunter',
+      src: 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/devilhunter_m.png',
+    },
+    {
+      kor: '블래스터',
+      eng: 'blaster',
+      src: 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/blaster_m.png',
+    },
+    {
+      kor: '호크아이',
+      eng: 'hawkeye',
+      src: 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/hawkeye_m.png',
+    },
+    {
+      kor: '스카우터',
+      eng: 'scouter',
+      src: 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/scouter_m.png',
+    },
+  ],
+
+  // 마법사
+  [
+    {
+      kor: '아르카나',
+      eng: 'arcana',
+      src: 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/arcana_m.png',
+    },
+    {
+      kor: '바드',
+      eng: 'bard',
+      src: 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/bard_m.png',
+    },
+    {
+      kor: '서머너',
+      eng: 'summoner',
+      src: 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/summoner_m.png',
+    },
+    {
+      kor: '소서리스',
+      eng: 'sorceress',
+      src: 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/sorceress_m.png',
+    },
+  ],
+  // 암살자
+  [
+    {
+      kor: '블레이드',
+      eng: 'blade',
+      src: 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/blade_m.png',
+    },
+    {
+      kor: '데모닉',
+      eng: 'demonic',
+      src: 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/demonic_m.png',
+    },
+    {
+      kor: '리퍼',
+      eng: 'reaper',
+      src: 'https://cdn-lostark.game.onstove.com/2018/obt/assets/images/common/thumb/reaper_m.png',
+    },
+  ],
+];
