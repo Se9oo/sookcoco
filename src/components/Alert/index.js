edd2e34ac7f46b17ec3aad0011ee54a2f9b36d2e
park se hyeon <se9oo@kakao.com>
2021-08-24 21:48:34 +0900

    alert 컴포넌트 생성

---
+++ src/components/Alert/index.js
@@ -0,0 +1,69 @@
+import React, { useRef } from 'react';
+
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  Button,
+  Flex,
+  Text,
+  Image,
+} from '@chakra-ui/react';
+import { WarningTwoIcon, InfoOutlineIcon } from '@chakra-ui/icons';
+
+const Alert = ({
+  isOpen,
+  setIsOpen,
+  message = '',
+  buttonText = '닫기',
+  kind,
+}) => {
+  const onClose = () => setIsOpen(false);
+  const cancelRef = useRef();
+
+  return (
+    <AlertDialog
+      isOpen={isOpen}
+      leastDestructiveRef={cancelRef}
+      onClose={onClose}
+    >
+      <AlertDialogOverlay>
+        <AlertDialogContent>
+          <AlertDialogHeader fontSize="lg" fontWeight="bold">
+            <Flex alignItems="center">
+              <Image
+                w="32px"
+                mr="5px"
+                src="/sookcoco-logo.png"
+                alt="sookcoco-logo"
+              />
+              <Text pt="5px">{kind}</Text>
+            </Flex>
+          </AlertDialogHeader>
+
+          <AlertDialogBody>
+            <Flex alignItems="center">
+              {kind === 'Error' ? (
+                <WarningTwoIcon mr="5px" mb="5px" color="red" />
+              ) : (
+                <InfoOutlineIcon mr="5px" mb="5px" color="blue.300" />
+              )}
+              {message}
+            </Flex>
+          </AlertDialogBody>
+
+          <AlertDialogFooter>
+            <Button ref={cancelRef} onClick={onClose}>
+              {buttonText}
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialogOverlay>
+    </AlertDialog>
+  );
+};
+
+export default Alert;
