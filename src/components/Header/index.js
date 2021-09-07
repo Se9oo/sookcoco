a58e236eb194718e9ce9ef4991c2ad250f6d8ad5
park se hyeon <se9oo@kakao.com>
2021-09-07 22:28:58 +0900

    header logo size 수정

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
-import { Box, Image } from '@chakra-ui/react';
-
-const width = ['32px', '48px', '48px', '50px', '50px'];
+import { Box, Image, useBreakpointValue } from '@chakra-ui/react';
 
 const Header = () => {
+  const size = useBreakpointValue({
+    xxs: '32px',
+    xs: '32px',
+    sm: '32px',
+    md: '48px',
+    lg: '48px',
+  });
+
   return (
     <Box w="100%" pt="5px" pb="5px" mb="2">
-      <Box w={width} m="0 auto">
+      <Box w={size} m="0 auto">
         <Image src="/sookcoco-logo.png" alt="sookcoco-logo" />
       </Box>
     </Box>
