e3dcbb42d635e8f6d834a00cb4c3b16de284f7d9
park se hyeon <se9oo@kakao.com>
2021-09-13 21:43:03 +0900

    캐릭터 추가/수정 form에 formik, yup 적용

--- src/components/Character/Modal/CharacterAddModal.js
+++ src/components/Character/Modal/CharacterAddModal.js
@@ -1,25 +1,19 @@
 import React, { useEffect, useState } from 'react';
 
 import {
-  Button,
   Flex,
   Image,
   Modal,
   ModalBody,
   ModalCloseButton,
   ModalContent,
-  ModalFooter,
   ModalHeader,
   ModalOverlay,
   useDisclosure,
 } from '@chakra-ui/react';
 
-import { schedule as commonSchedule } from '../../../common/common';
-
-import useInput from '../../../hooks/useInput';
 import CharacterForm from './CharacterForm';
 import CharacterSelectModal from './CharacterSelectModal';
-import Alert from '../../Alert';
 
 const CharacterAddModal = ({
   open,
@@ -30,25 +24,9 @@ const CharacterAddModal = ({
 }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const [server, onChangeServer] = useInput(
-    mode === 'update' && characterInfo ? characterInfo.server : ''
-  );
   const [selectClass, setSelectClass] = useState(
     mode === 'update' && characterInfo ? characterInfo.selectClass : ''
   );
-  const [name, onChangeName] = useInput(
-    mode === 'update' && characterInfo ? characterInfo.name : ''
-  );
-  const [level, onChangeLevel] = useInput(
-    mode === 'update' && characterInfo ? characterInfo.level : ''
-  );
-  const [itemLevel, onChangeItemLevel] = useInput(
-    mode === 'update' && characterInfo ? characterInfo.itemLevel : ''
-  );
-
-  const [alertOpen, setAlertOpen] = useState(false);
-  const [alertMsg, setAlertMsg] = useState('');
-  const [alertKind, setAlertKind] = useState('');
 
   // modal이 다시 열리면 selectClass 초기화
   useEffect(() => {
@@ -56,93 +34,6 @@ const CharacterAddModal = ({
       setSelectClass('');
     }
   }, [open]);
-
-  // 캐릭터 추가
-  const onClickAddBtn = () => {
-    if (!server || !selectClass || !name || !level || !itemLevel) {
-      setAlertMsg('입력 값을 확인해주세요!');
-      setAlertKind('Error');
-      setAlertOpen(true);
-      return;
-    }
-
-    const data = { server, selectClass, name, level, itemLevel };
-    // 키 값 default
-    data.characterKey = 0;
-
-    const origin = JSON.parse(window.localStorage.getItem('sookcoco'));
-
-    // sookcoco key가 존재하지 않으면 (첫 캐릭터 생성시)
-    if (!origin) {
-      const userObj = {
-        characters: [data],
-        expedition: commonSchedule.expedition,
-      };
-
-      userObj.characters[0].characterKey = 0;
-
-      window.localStorage.setItem('sookcoco', JSON.stringify(userObj));
-      setCharacterList([data]);
-    } else {
-      if (origin !== null && origin.hasOwnProperty('characters')) {
-        data.characterKey = origin.characters.length;
-        origin.characters.push(data);
-      } else {
-        origin.characters = [{ data }];
-      }
-
-      window.localStorage.setItem('sookcoco', JSON.stringify(origin));
-      setCharacterList([...origin.characters]);
-    }
-
-    close();
-  };
-
-  // 캐릭터 수정
-  const onClickUpdateBtn = () => {
-    if (!server || !selectClass || !name || !level || !itemLevel) {
-      setAlertMsg('입력 값을 확인해주세요!');
-      setAlertKind('Error');
-      setAlertOpen(true);
-      return;
-    }
-
-    const origin = JSON.parse(window.localStorage.getItem('sookcoco'));
-
-    const updateStorage = origin.characters.map((data) => {
-      if (data.characterKey === characterInfo.characterKey) {
-        data.name = name;
-        data.level = level;
-        data.itemLevel = itemLevel;
-      }
-      return data;
-    });
-
-    origin.characters = updateStorage;
-
-    window.localStorage.setItem('sookcoco', JSON.stringify(origin));
-
-    setCharacterList([...updateStorage]);
-
-    close();
-  };
-
-  // 캐릭터 삭제
-  const onClickDeleteBtn = () => {
-    const origin = JSON.parse(window.localStorage.getItem('sookcoco'));
-
-    const deleteIdx = origin.characters.findIndex(
-      (data) => data.characterKey === characterInfo.characterKey
-    );
-
-    origin.characters.splice(deleteIdx, 1);
-
-    window.localStorage.setItem('sookcoco', JSON.stringify(origin));
-
-    setCharacterList([...origin.characters]);
-
-    close();
-  };
 
   return (
     <>
@@ -165,45 +56,19 @@ const CharacterAddModal = ({
           <ModalBody pb={3}>
             <CharacterForm
               onOpen={onOpen}
-              onChangeServer={onChangeServer}
               selectClass={selectClass}
-              onChangeName={onChangeName}
-              onChangeLevel={onChangeLevel}
-              onChangeItemLevel={onChangeItemLevel}
               characterInfo={characterInfo}
+              setCharacterList={setCharacterList}
               mode={mode}
+              close={close}
             />
           </ModalBody>
-
-          <ModalFooter>
-            {mode === 'update' ? (
-              <>
-                <Button colorScheme="red" mr="5px" onClick={onClickDeleteBtn}>
-                  삭제
-                </Button>
-                <Button colorScheme="green" mr="5px" onClick={onClickUpdateBtn}>
-                  수정
-                </Button>
-              </>
-            ) : (
-              <Button colorScheme="green" mr="5px" onClick={onClickAddBtn}>
-                추가
-              </Button>
-            )}
-            <Button onClick={close}>취소</Button>
-          </ModalFooter>
         </ModalContent>
       </Modal>
       <CharacterSelectModal
         isOpen={isOpen}
         onClose={onClose}
         setSelectClass={setSelectClass}
-      />
-      <Alert
-        isOpen={alertOpen}
-        setIsOpen={setAlertOpen}
-        message={alertMsg}
-        kind={alertKind}
       />
     </>
   );
--- src/components/Character/Modal/CharacterForm.js
+++ src/components/Character/Modal/CharacterForm.js
@@ -7,88 +7,257 @@ import {
   InputLeftAddon,
   Input,
   Select,
+  Button,
+  Flex,
+  FormErrorMessage,
 } from '@chakra-ui/react';
 
-import { servers } from '../../../common/common';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+
+import { servers, schedule as commonSchedule } from '../../../common/common';
 
 const CharacterForm = (props) => {
-  const {
-    onOpen,
-    onChangeServer,
-    selectClass,
-    onChangeName,
-    onChangeLevel,
-    onChangeItemLevel,
-    characterInfo,
-    mode,
-  } = props;
+  const { onOpen, selectClass, characterInfo, setCharacterList, mode, close } =
+    props;
+
+  const numberInputFormat = (e) => {
+    let checkNum;
+    if (e.key !== undefined) {
+      checkNum =
+        e.key === 'e' || e.key === '.' || e.key === '+' || e.key === '-';
+    } else if (e.keyCode !== undefined) {
+      checkNum =
+        e.keyCode === 69 ||
+        e.keyCode === 190 ||
+        e.keyCode === 187 ||
+        e.keyCode === 189;
+    }
+
+    return checkNum && e.preventDefault();
+  };
+
+  // form validation
+  const validationSchema = Yup.object().shape({
+    server: Yup.string().required('서버를 선택해 주세요.'),
+    name: Yup.string()
+      .max(12, '이름은 최대 12자 입니다.')
+      .required('이름을 입력해 주세요.'),
+    level: Yup.number()
+      .min(1, '최소 레벨은 1 입니다.')
+      .max(60, '최대 레벨은 60 입니다.')
+      .required('레벨을 입력해 주세요.'),
+    itemLevel: Yup.number()
+      .max(2000, '아이템 레벨은 2000 이하로 설정 가능합니다.')
+      .required('아이템 레벨을 입력해 주세요.'),
+  });
+
+  // 캐릭터 추가/수정
+  const onSubmitForm = (data, { setSubmitting }) => {
+    setSubmitting(true);
+    const origin = JSON.parse(window.localStorage.getItem('sookcoco'));
+
+    if (mode !== 'update') {
+      data.characterKey = 0;
+      data.selectClass = selectClass;
+
+      // sookcoco key가 존재하지 않으면 (첫 캐릭터 생성시)
+      if (!origin) {
+        const userObj = {
+          characters: [data],
+          expedition: commonSchedule.expedition,
+        };
+
+        userObj.characters[0].characterKey = 0;
+
+        window.localStorage.setItem('sookcoco', JSON.stringify(userObj));
+        setCharacterList([data]);
+      } else {
+        if (origin !== null && origin.hasOwnProperty('characters')) {
+          data.characterKey = origin.characters.length;
+          origin.characters.push(data);
+        } else {
+          origin.characters = [{ data }];
+        }
+
+        window.localStorage.setItem('sookcoco', JSON.stringify(origin));
+        setCharacterList([...origin.characters]);
+      }
+    } else {
+      origin.characters.map((character) => {
+        if (character.characterKey === characterInfo.characterKey) {
+          character.name = data.name;
+          character.level = data.level;
+          character.itemLevel = data.itemLevel;
+        }
+
+        return data;
+      });
+
+      window.localStorage.setItem('sookcoco', JSON.stringify(origin));
+
+      setCharacterList([...origin.characters]);
+    }
+
+    setSubmitting(false);
+
+    close();
+  };
+
+  // 캐릭터 삭제
+  const onClickDeleteBtn = () => {
+    const origin = JSON.parse(window.localStorage.getItem('sookcoco'));
+
+    const deleteIdx = origin.characters.findIndex(
+      (character) => character.characterKey === characterInfo.characterKey
+    );
+
+    origin.characters.splice(deleteIdx, 1);
+
+    window.localStorage.setItem('sookcoco', JSON.stringify(origin));
+
+    setCharacterList([...origin.characters]);
+
+    close();
+  };
 
   return (
-    <>
-      <FormControl mb={4} isRequired={mode === 'update' ? false : true}>
-        <FormLabel>서버</FormLabel>
-        <Select
-          focusBorderColor="green.500"
-          placeholder="서버를 선택해주세요."
-          onChange={(option) => onChangeServer(option)}
-          disabled={mode === 'update' ? true : false}
-          defaultValue={characterInfo ? characterInfo.server : ''}
-        >
-          {servers.map((server) => (
-            <option key={server.eng} value={server.eng}>
-              {server.kor}
-            </option>
-          ))}
-        </Select>
-      </FormControl>
-
-      <FormControl mb={4} isRequired={mode === 'update' ? false : true}>
-        <FormLabel>클래스</FormLabel>
-        <Input
-          focusBorderColor="green.500"
-          placeholder="클래스를 선택해주세요."
-          onClick={onOpen}
-          value={selectClass}
-          isReadOnly
-          disabled={mode === 'update' ? true : false}
-        />
-      </FormControl>
-
-      <FormControl mb={4} isRequired>
-        <FormLabel>캐릭터 이름</FormLabel>
-        <Input
-          focusBorderColor="green.500"
-          placeholder="캐릭터 이름"
-          onChange={onChangeName}
-          defaultValue={characterInfo ? characterInfo.name : null}
-        />
-      </FormControl>
-
-      <FormControl mb={4} isRequired>
-        <FormLabel>캐릭터 레벨</FormLabel>
-        <InputGroup>
-          <InputLeftAddon children="Lv." />
-          <Input
-            type="number"
-            focusBorderColor="green.500"
-            placeholder="캐릭터 레벨"
-            onChange={onChangeLevel}
-            defaultValue={characterInfo ? characterInfo.level : null}
-          />
-        </InputGroup>
-      </FormControl>
-
-      <FormControl isRequired>
-        <FormLabel>아이템 레벨</FormLabel>
-        <Input
-          type="number"
-          focusBorderColor="green.500"
-          placeholder="아이템 레벨"
-          onChange={onChangeItemLevel}
-          defaultValue={characterInfo ? characterInfo.itemLevel : null}
-        />
-      </FormControl>
-    </>
+    <Formik
+      initialValues={{
+        server: characterInfo ? characterInfo.server : '',
+        name: characterInfo ? characterInfo.name : '',
+        level: characterInfo ? characterInfo.level : '',
+        itemLevel: characterInfo ? characterInfo.itemLevel : '',
+      }}
+      validationSchema={validationSchema}
+      onSubmit={onSubmitForm}
+    >
+      {({ values, handleChange }) => (
+        <Form>
+          <Field name="server">
+            {({ field, form }) => (
+              <FormControl
+                mb={4}
+                isRequired={mode === 'update' ? false : true}
+                isInvalid={form.errors.server && form.touched.server}
+              >
+                <FormLabel>서버</FormLabel>
+                <Select
+                  name="server"
+                  {...field}
+                  focusBorderColor="green.500"
+                  placeholder="서버를 선택해주세요."
+                  onChange={handleChange}
+                  disabled={mode === 'update' ? true : false}
+                  value={values.server}
+                >
+                  {servers.map((server) => (
+                    <option key={server.eng} value={server.eng}>
+                      {server.kor}
+                    </option>
+                  ))}
+                </Select>
+                <FormErrorMessage>{form.errors.server}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+          <FormControl mb={4} isRequired={mode === 'update' ? false : true}>
+            <FormLabel>클래스</FormLabel>
+            <Input
+              name="selectClass"
+              focusBorderColor="green.500"
+              placeholder="클래스를 선택해주세요."
+              onClick={onOpen}
+              value={selectClass}
+              isReadOnly
+              disabled={mode === 'update' ? true : false}
+            />
+          </FormControl>
+          <Field name="name">
+            {({ field, form }) => (
+              <FormControl
+                mb={4}
+                isRequired
+                isInvalid={form.errors.name && form.touched.name}
+              >
+                <FormLabel>캐릭터 이름</FormLabel>
+                <Input
+                  name="name"
+                  {...field}
+                  focusBorderColor="green.500"
+                  placeholder="캐릭터 이름"
+                  onChange={handleChange}
+                  value={values.name}
+                />
+                <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+          <Field name="level">
+            {({ field, form }) => (
+              <FormControl
+                mb={4}
+                isRequired
+                isInvalid={form.errors.level && form.touched.level}
+              >
+                <FormLabel>캐릭터 레벨</FormLabel>
+                <InputGroup>
+                  <InputLeftAddon children="Lv." />
+                  <Input
+                    type="number"
+                    {...field}
+                    name="level"
+                    focusBorderColor="green.500"
+                    placeholder="캐릭터 레벨"
+                    onChange={handleChange}
+                    onKeyDown={numberInputFormat}
+                    value={values.level}
+                    min="0"
+                    max="60"
+                  />
+                </InputGroup>
+                <FormErrorMessage>{form.errors.level}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+          <Field name="itemLevel">
+            {({ field, form }) => (
+              <FormControl
+                mb={4}
+                isRequired
+                isInvalid={form.errors.itemLevel && form.touched.itemLevel}
+              >
+                <FormLabel>아이템 레벨</FormLabel>
+                <Input
+                  type="number"
+                  name="itemLevel"
+                  {...field}
+                  focusBorderColor="green.500"
+                  placeholder="아이템 레벨"
+                  onChange={handleChange}
+                  onKeyDown={numberInputFormat}
+                  value={values.itemLevel}
+                  min="0"
+                  max="2000"
+                />
+                <FormErrorMessage>{form.errors.itemLevel}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+          <Flex justifyContent="flex-end">
+            {mode === 'update' && (
+              <Button colorScheme="red" mr="5px" onClick={onClickDeleteBtn}>
+                삭제
+              </Button>
+            )}
+            <Button type="submit" colorScheme="green" mr="5px">
+              {mode === 'update' ? '수정' : '추가'}
+            </Button>
+            <Button onClick={close}>취소</Button>
+          </Flex>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
