9f8cab869b43ad89c918f7a2bf34423a0accb6cd
park se hyeon <se9oo@kakao.com>
2021-09-14 02:18:40 +0900

    스케줄 초기화 로직 추가

--- src/Sookcoco.js
+++ src/Sookcoco.js
@@ -18,6 +18,62 @@ const Sookcoco = () => {
     expedition: [],
   });
 
+  // localStorage 스케줄 관련
+  useEffect(() => {
+    const now = new Date();
+    const year = String(now.getFullYear());
+    let month = now.getMonth() + 1;
+    let date = now.getDate();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+
+    month = month >= 10 ? month : '0' + month;
+    date = date >= 10 ? date : '0' + date;
+    hour = hour >= 10 ? hour : '0' + hour;
+    minute = minute >= 10 ? minute : '0' + minute;
+
+    const today = year + month + date + hour + minute;
+
+    let lastConnect = JSON.parse(window.localStorage.getItem('lastConnect'));
+
+    if (!lastConnect) {
+      lastConnect = today;
+    }
+
+    // 매일 06시
+    const standard = year + month + date + '0600';
+
+    //
+    if (parseInt(lastConnect) <= parseInt(standard)) {
+      const origin = JSON.parse(window.localStorage.getItem('sookcoco'));
+
+      origin.characters.map((character) => {
+        if (character.hasOwnProperty('schedule')) {
+          schedule.daily.length > 0 &&
+            schedule.daily.map((day) => {
+              day.done = 0;
+            });
+
+          if (now.getDay() === 3) {
+            schedule.weekly.length > 0 &&
+              schedule.weekly.map((week) => {
+                week.done = 0;
+              });
+          }
+        }
+      });
+
+      if (now.getDay() === 3 && origin.expedition.length > 0) {
+        origin.expedition.map((exp) => {
+          exp.done = 0;
+        });
+      }
+
+      window.localStorage.setItem('sookcoco', JSON.stringify(origin));
+      window.localStorage.setItem('lastConnect', JSON.stringify(today));
+    }
+  }, []);
+
   // 화면 진입시
   useEffect(() => {
     const data = JSON.parse(window.localStorage.getItem('sookcoco'));
