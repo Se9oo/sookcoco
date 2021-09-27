003d2ae29f1689f33bf938db91aa4c48d18f2f04
park se hyeon <se9oo@kakao.com>
2021-09-27 23:18:50 +0900

    copyright update

--- src/components/Header/Copyright.js
+++ src/components/Header/Copyright.js
@@ -5,7 +5,11 @@ import { Box, Flex, Text } from '@chakra-ui/react';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import dayjs from 'dayjs';
+
 const Copyright = () => {
+  const now = dayjs().format('YYYY');
+
   const onClickGitHub = () => {
     window.open('https://github.com/Se9oo/sookcoco', '_blank');
   };
@@ -16,9 +20,10 @@ const Copyright = () => {
       textAlign="center"
       justifyContent="center"
       alignItems="center"
-      fontSize="xs"
     >
-      <Text mb="5px">&copy; 2021. Park Se Hyun All rights reserved.</Text>
+      <Text fontSize="xs" mb="5px">
+        &copy; {`${now}. se9oo All rights reserved.`}
+      </Text>
       <Box
         css={{
           '&:hover': {
@@ -28,7 +33,7 @@ const Copyright = () => {
       >
         <FontAwesomeIcon
           icon={faGithub}
-          size="2x"
+          size="lg"
           cursor="pointer"
           onClick={onClickGitHub}
         />
