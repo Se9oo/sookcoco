5d90b22d75b9e875742d82f9a3a5a2840a7d81ec
park se hyeon <se9oo@kakao.com>
2021-09-27 23:18:37 +0900

    선택하지 않은 캐릭터 카드만 hover 되도록 수정

--- src/components/Character/CharacterCard.js
+++ src/components/Character/CharacterCard.js
@@ -78,7 +78,7 @@ const CharacterCard = ({
         selectCharacter === characterKey ? 'rgba(148, 216, 45, 1)' : 'gray.50'
       }
       cursor="pointer"
-      _hover={{ bg: 'gray.100' }}
+      _hover={selectCharacter !== characterKey ? { bg: 'gray.100' } : null}
       flexDirection={direction}
       onClick={onClickCharacterCard(characterKey)}
     >
