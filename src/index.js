c584da6dd4709f1b2ecf25e1579c97496dfe8e4b
park se hyeon <se9oo@kakao.com>
2021-10-05 18:35:42 +0900

    react-snap 추가

--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Sookcoco from './Sookcoco';
 
+const rootElement = document.getElementById('root');
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(
+    <React.StrictMode>
+      <Sookcoco />
+    </React.StrictMode>,
+    rootElement
+  );
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Sookcoco />
+    </React.StrictMode>,
+    rootElement
+  );
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <Sookcoco />
