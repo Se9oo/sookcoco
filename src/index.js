050f5ecf329315944a7b86a0d86250f30e64aad6
park se hyeon <se9oo@kakao.com>
2021-10-06 15:49:08 +0900

    react snap 원복

--- src/index.js
+++ src/index.js
@@ -2,20 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Sookcoco from './Sookcoco';
 
-const rootElement = document.getElementById('root');
-
-if (rootElement.hasChildNodes()) {
-  ReactDOM.hydrate(
-    <React.StrictMode>
-      <Sookcoco />
-    </React.StrictMode>,
-    rootElement
-  );
-} else {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Sookcoco />
-    </React.StrictMode>,
-    rootElement
-  );
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <Sookcoco />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
