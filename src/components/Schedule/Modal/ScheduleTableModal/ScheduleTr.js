a40c6ffc4055f7da7679598461ac05192a29d960
park se hyeon <se9oo@kakao.com>
2021-10-14 18:11:41 +0900

    전체 스케줄 현황 > 캐릭터 이름 반응형 처리

--- src/components/Schedule/Modal/ScheduleTableModal/ScheduleTr.js
+++ src/components/Schedule/Modal/ScheduleTableModal/ScheduleTr.js
@@ -3,10 +3,19 @@ import React from 'react';
 import { Avatar } from '@chakra-ui/avatar';
 import { Flex, Text } from '@chakra-ui/layout';
 import { Td, Tr } from '@chakra-ui/table';
+import { useBreakpointValue } from '@chakra-ui/media-query';
 
 import ScheduleTd from './ScheduleTd';
 
 const ScheduleTr = ({ schedule }) => {
+  const isDisplay = useBreakpointValue({
+    xxs: 'none',
+    xs: 'none',
+    sm: 'none',
+    md: 'block',
+    lg: 'block',
+  });
+
   return (
     <Tr>
       <Td>
@@ -18,7 +27,7 @@ const ScheduleTr = ({ schedule }) => {
             bg="transparent"
             mb="5px"
           />
-          <Text>{schedule[0].name}</Text>
+          <Text display={isDisplay}>{schedule[0].name}</Text>
         </Flex>
       </Td>
       {schedule.map((sch, idx) => {
