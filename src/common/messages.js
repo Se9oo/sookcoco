f91105d64627a8efe7e607616b633e3eedd9e607
park se hyeon <se9oo@kakao.com>
2022-05-29 20:42:59 +0900

    alert message 별도 파일로 분리

---
+++ src/common/messages.js
@@ -0,0 +1,18 @@
+// alert messages
+export const backUpDataMessage = '클립보드 복사 완료';
+export const loadDataMessage =
+  '데이터 불러오기 완료\n닫기를 누르면 페이지가 새로고침 됩니다.';
+
+export const loadDataFailMessage = '데이터 불러오기 실패';
+
+export const clearDataMessage =
+  '초기화 된 데이터는 복구 할 수 없습니다.\n초기화 하시겠습니까?';
+
+export const updateContentMessage =
+  '컨텐츠를 최신 정보로 반영합니다.\n업데이트를 누르면 페이지가 새로고침 됩니다.';
+
+export const demoDataMessage =
+  '데모 데이터를 적용합니다.\n\n기존 데이터는 덮어씌워지므로 반드시 백업해주세요!';
+
+export const helpMessage =
+  '처음 접속하셨군요!\n\n우측 상단 메뉴를 눌러 데모 데이터를 적용해보세요!';
